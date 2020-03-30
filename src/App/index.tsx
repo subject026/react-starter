c412a82e34ae1b2c42690c9f05c33bac847d15b7
subject026 <sudobuildspeakers@gmail.com>
2020-03-30 12:21:04 +0100

    hmm

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,12 +5,21 @@ const Title = styled.h1`
   font-size: 100px;
 `;
 
+type TThingProps = {
+  word: string;
+};
+
+const Thing: React.FC<TThingProps> = props => {
+  return <h1>{props.word}</h1>;
+};
+
 export default (): React.ReactElement => {
   console.error("BOOM");
   return (
     <div>
       <Title>app</Title>
       <p>Wooooooo</p>
+      <Thing word="cheeses" />
     </div>
   );
 };
